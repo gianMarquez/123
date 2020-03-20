e865f46b7559a7b873b2b04e651b063afc802c2d
grandcanyonsmith <canyon@bottega.tech>
2020-03-20 16:35:45 -0600

    make it better

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component }  from 'react';
+import {BrowserRouter as Router, Switch,Route } from 'react-router-dom';
+import Home from './screens/home'
+import About from './screens/about'
+import Login from './screens/login'
+import Info from './screens/info'
+import './media.css'
+class App extends Component { 
+  render (){
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/"  component={Home}/>
+          <Route exact path="/aboutus" component={About} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/info" component={Info} />
+        </Switch>
+      </Router>
+    );
+  }
+  
+}
+
+export default App;
